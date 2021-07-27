54f7c28816a4abddb22619192ae569ff13041a83
Devesh Sarda <32046390+sarda-devesh@users.noreply.github.com>
2021-07-27 11:19:31 -0700

    Create cache file for testing

--- src/Program.cs
+++ src/Program.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Reflection;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Composition;
 using VSMefx.Commands;
-using System.CommandLine.DragonFruit;
 
 namespace VSMefx
 {
